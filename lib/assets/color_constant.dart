2b73858548dcc1b4cd892e0d4117f782e78cff23
runnsss <s.runn@ext-lmwn.com>
2023-01-15 17:03:24 +0700

    [constant] Add color constant

---
+++ lib/assets/color_constant.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class ColorConstant {
+  final orange400B =
+      const TextStyle(fontWeight: FontWeight.bold, color: Color(0xFFFFA726));
+  final orange500B =
+      const TextStyle(fontWeight: FontWeight.bold, color: Color(0xFFFF9800));
+}
