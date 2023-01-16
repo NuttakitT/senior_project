cee9bcaa3f787f5e8b92f194dac1b3da39cad1da
NuttakitT <nuttakit.pliankunthod@gmail.com>
2023-01-16 16:59:51 +0700

    add constants

--- lib/assets/constant.dart
+++ lib/assets/constant.dart
@@ -5,8 +5,11 @@ class Constant {
   static const facebookColor = Color(0xFF4267B2);
   static const orange5 = Color(0xFFFFF3E0);
   static const orange40 = Color(0xFFFFA726);
+  static const orange50 = Color(0xFFFF9800);
   static const orange70 = Color(0xFFF57C00);
   static const orange90 = Color(0xFFE65100);
+  static const whiteBlack15 = Color(0xFFDADBDC);
+  static const whiteBlack20 = Color(0xFFCECFD0);
   static const whiteBlack30 = Color(0xFFB5B7B8);
   static const whiteBlack40 = Color(0xFF9C9FA1);
   static const whiteBlack50 = Color(0xFF838689);
