6485d7f1dd45f4ac9072ef9dcc1a2c8ea2007c01
NuttakitT <nuttakit.pliankunthod@gmail.com>
2023-01-23 14:50:48 +0700

    clear comments

--- lib/main.dart
+++ lib/main.dart
@@ -7,8 +7,6 @@ import 'package:senior_project/user_authentication/login_register_page/view_mode
 import 'package:senior_project/user_authentication/login_register_page/view_model/register_view_model.dart';
 import 'package:senior_project/user_authentication/role_selection_page/view_model/role_selection_view_model.dart';
 import 'firebase_options.dart';
-// * test ceck web on desktop/mobile size
-// * import 'package:flutter/foundation.dart';
 
 void main() async {
   await Firebase.initializeApp(
@@ -32,10 +30,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // * test ceck web on desktop/mobile size
-    // * final isWebMobile = kIsWeb && (defaultTargetPlatform == TargetPlatform.iOS || defaultTargetPlatform == TargetPlatform.android); 
-    // * print("log: web is on mobile /$isWebMobile/ ");
-
     return MaterialApp(
       title: 'Test',
       theme: ThemeData(
