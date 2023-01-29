a43937c52ff80bcdeb71a91325b5a38876d3d629
runnsss <s.runn@ext-lmwn.com>
2023-01-29 13:21:26 +0700

    create Halp desk page for web

--- lib/help_desk/help_desk_main/model/help_desk_main_model.dart
+++ lib/help_desk/help_desk_main/model/help_desk_main_model.dart
@@ -1,1 +1,32 @@
 class HelpDeskModel {}
+
+// class HelpDeskCard {
+//   String title;
+//   String category;
+//   String detail;
+//   String priority;
+// }
+
+// const data = [
+//   {
+//     "title": "title01",
+//     "category": "Room jeng",
+//     "detail":
+//         "Lorem ipsum grhbnujibnrogbibveifgwyvbiewvgviowevyueovfuyfsvuyfvwvu",
+//     "priority": "high"
+//   },
+//   {
+//     "title": "title02",
+//     "category": "Roommmmmm jeng",
+//     "detail":
+//         "Lorem ipsum grhbnujibnrogbibveifgwyvbiewvgviowevyueovfuyfsvuyfvwvu",
+//     "priority": "high"
+//   },
+//   {
+//     "title": "title03",
+//     "category": "GGGGGGGGGJGJGJGJGJGJGJGJG",
+//     "detail":
+//         "Lorem ipsum grhbnujibnrogbibveifgwyvbiewvgviowevyueovfuyfsvuyfvwvu",
+//     "priority": "high"
+//   },
+// ];
--- lib/help_desk/help_desk_main/view/help_desk_main_view.dart
+++ lib/help_desk/help_desk_main/view/help_desk_main_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:senior_project/assets/color_constant.dart';
 import 'package:senior_project/help_desk/help_desk_main/view_model/help_desk_main_view_model.dart';
 
 class HelpDeskMainView extends StatefulWidget {
@@ -17,10 +18,152 @@ class _HelpDeskMainViewState extends State<HelpDeskMainView> {
     super.initState();
   }
 
+  List helpDeskCard = [
+    "Oh nana",
+    "Oh Asok",
+    "Oh nana",
+    "Oh Asok",
+    "Oh nana",
+    "Oh Asok",
+    "Oh nana",
+    "Oh Asok",
+    "Oh nana",
+    "Oh Asok",
+    "Oh nana",
+    "Oh Asok",
+    "Oh nana",
+    "Oh Asok",
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text(viewModel.getName()),
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              decoration: const BoxDecoration(color: Colors.transparent),
+              child: SizedBox(
+                width: MediaQuery.of(context).size.width,
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(32.0, 16.0, 12.0, 32.0),
+                  child: Row(
+                    children: [
+                      Text('Help Desk', style: ColorConstant().blackMd38),
+                      const Spacer(),
+                      TextButton(
+                        child: Text('+ Create Task',
+                            style: ColorConstant().orange500B16),
+                        onPressed: () {},
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Container(
+            height: MediaQuery.of(context).size.height,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ListView.builder(
+                itemCount: helpDeskCard.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      decoration: BoxDecoration(
+                          color: Colors.grey[50],
+                          borderRadius:
+                              const BorderRadius.all(Radius.circular(8.0))),
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Row(
+                          children: [
+                            SizedBox(
+                              width: 125,
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                crossAxisAlignment: CrossAxisAlignment.end,
+                                children: [
+                                  Text("Urgent!!",
+                                      style: ColorConstant().orange500B16),
+                                  Text("Category:",
+                                      style: ColorConstant().blackL20),
+                                  Text("Detail:",
+                                      style: ColorConstant().blackL20)
+                                ],
+                              ),
+                            ),
+                            const SizedBox(width: 8),
+                            SizedBox(
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(helpDeskCard[index],
+                                      style: ColorConstant().blackMd24),
+                                  Text(helpDeskCard[index],
+                                      style: ColorConstant().blackMd20),
+                                  Container(
+                                    width: 500, //TODO: Fix width
+                                    decoration: BoxDecoration(
+                                        color: Colors.white,
+                                        borderRadius: const BorderRadius.all(
+                                            Radius.circular(4.0)),
+                                        border:
+                                            Border.all(color: Colors.black)),
+                                    child: Padding(
+                                      padding: EdgeInsets.all(4.0),
+                                      child: Text(helpDeskCard[index],
+                                          style: ColorConstant().grey50Md16),
+                                    ),
+                                  )
+                                ],
+                              ),
+                            ),
+                            const SizedBox(width: 8),
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              crossAxisAlignment: CrossAxisAlignment.end,
+                              children: [
+                                Text("Time is ",
+                                    style: ColorConstant().blackMd18),
+                                ButtonWithText(text: "Message")
+                              ],
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ),
+        ],
+      ),
     );
+  }
+}
+
+class ButtonWithText extends StatelessWidget {
+  const ButtonWithText({Key? key, required this.text}) : super(key: key);
+
+  final String text;
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+        child: Container(
+          decoration: const BoxDecoration(
+              color: Colors.orange,
+              borderRadius: BorderRadius.all(Radius.circular(4.0))),
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(71, 12, 71, 12),
+            child: Text(text, style: ColorConstant().white14B),
+          ),
+        ),
+        onPressed: () {});
   }
 }
