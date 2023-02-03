2c2f42ed133e030e6250a03540fa2ca443d989cb
NuttakitT <nuttakit.pliankunthod@gmail.com>
2023-02-03 11:40:22 +0700

    edited alignment mobile main page

--- lib/help_desk/admin/help_desk_main/view/widget/mobile/mobile_widget.dart
+++ lib/help_desk/admin/help_desk_main/view/widget/mobile/mobile_widget.dart
@@ -48,12 +48,14 @@ class _MobileWidgetState extends State<MobileWidget> {
     },
   ];
   
-  ScrollController _vontraoller = ScrollController();
+  final ScrollController _vContraoller = ScrollController();
+  final ScrollController _menuController = ScrollController();
 
   Widget menu(String name, bool state) {
     return Container(
       padding: const EdgeInsets.fromLTRB(16, 0, 16,5),
       decoration: BoxDecoration(
+        color: Colors.white,
         border: state 
         ? const Border(
           bottom: BorderSide(
@@ -75,40 +77,59 @@ class _MobileWidgetState extends State<MobileWidget> {
     );
   }
 
+  double _scaleText(double pixelWidth) {
+    if (pixelWidth <= 300 && pixelWidth > 250) {
+      return -4;
+    }
+    if (pixelWidth <= 250) {
+      return -6;
+    }
+    return 0;
+  }
+
   @override
   Widget build(BuildContext context) {
-
+    double pixelWidth = MediaQuery.of(context).size.width;
     // TODO edit templete w/ scrollable
     return Column(
       children: [
         Container(
-          padding: const EdgeInsets.only(top: 24, bottom: 36),
+          color: Colors.white,
+          padding: EdgeInsets.only(top: 24 + _scaleText(pixelWidth), bottom: 30 + _scaleText(pixelWidth)),
           alignment: Alignment.center,
           width: double.infinity,
           child: Stack(
+            alignment: AlignmentDirectional.center,
             children: [
-              const Align(
-                alignment: Alignment.center,
-                child: Text(
-                  "Help-desk List",
-                  style: TextStyle(
-                    fontFamily: ColorConstant.font,
-                    fontWeight: FontWeight.w500,
-                    fontSize: 28,
-                    color: ColorConstant.whiteBlack80
+              Padding(
+                padding: EdgeInsets.all(pixelWidth < 260  ? 10 :0),
+                child: Align(
+                  alignment: pixelWidth < 260 
+                    ? AlignmentDirectional.centerStart 
+                    : AlignmentDirectional.center,
+                  child: Text(
+                    "Help-desk List",
+                    style: TextStyle(
+                      fontFamily: ColorConstant.font,
+                      fontWeight: FontWeight.w500,
+                      fontSize: 28 + _scaleText(pixelWidth),
+                      color: ColorConstant.whiteBlack80
+                    ),
                   ),
                 ),
               ),
               Align(
-                alignment: Alignment.centerRight,
+                alignment: AlignmentDirectional.centerEnd,
                 child: IconButton(
                   onPressed: () {
                     // TODO pop up setting
                   }, 
-                  alignment: Alignment.centerRight,
+                  padding: EdgeInsets.only(right: 18.25 + _scaleText(pixelWidth)),
+                  iconSize: 28 + _scaleText(pixelWidth),
+                  // alignment: Alignment.centerRight,
                   color: ColorConstant.whiteBlack80,
                   icon: const Icon(
-                    Icons.settings_rounded
+                    Icons.settings_outlined
                   )
                 ),
               )
@@ -118,31 +139,57 @@ class _MobileWidgetState extends State<MobileWidget> {
         Container(
           width: double.infinity,
           decoration: const BoxDecoration(
+            color: Colors.white,
             border: Border(
               bottom: BorderSide(
                 color: ColorConstant.whiteBlack15
               )
             )
           ),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              // TODO listen to provider state
-              Flexible(flex: 2, child: menu("All", true)),
-              Flexible(flex: 4, child: menu("Not start", false)),
-              Flexible(flex: 4, child: menu("In progress", false)),
-              Flexible(flex: 3, child: menu("Closed", false)),
-            ],
+          child: Builder(
+            builder: (context) {
+              if (pixelWidth <= 340) {
+                return Scrollbar(
+                  controller: _menuController,
+                  thumbVisibility: true,
+                  thickness: 4,
+                  child: SingleChildScrollView(
+                    controller: _menuController,
+                    scrollDirection: Axis.horizontal,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        // TODO listen to provider state
+                        menu("All", true),
+                        menu("Not start", false),
+                        menu("In progress", false),
+                        menu("Closed", false),
+                      ],
+                    ),
+                  ),
+                );
+              }
+              return Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  // TODO listen to provider state
+                  menu("All", true),
+                  menu("Not start", false),
+                  menu("In progress", false),
+                  menu("Closed", false),
+                ],
+              );
+            }
           ),
         ),
         Expanded(
           child: Padding(
             padding: const EdgeInsets.only(top: 24),
             child: Scrollbar(
-              controller: _vontraoller,
+              controller: _vContraoller,
               thumbVisibility: true,
               child: SingleChildScrollView(
-                controller: _vontraoller,
+                controller: _vContraoller,
                 child: Column(
                   children: [
                     TaskCard(detail: data[0]),
