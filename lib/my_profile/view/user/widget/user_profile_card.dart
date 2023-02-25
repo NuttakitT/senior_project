a2f46c2e65c78c102941c02d9230399047756c6f
runnsss <s.runn@ext-lmwn.com>
2023-02-25 13:22:57 +0700

    Fix font style.

--- lib/my_profile/view/user/widget/user_profile_card.dart
+++ lib/my_profile/view/user/widget/user_profile_card.dart
@@ -1,32 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:senior_project/assets/color_constant.dart';
 import 'package:senior_project/assets/font_style.dart';
 
 class UserProfileCard {
-  static TextStyle nameTextStyle() => const TextStyle(
-      fontFamily: AppFontStyle.font,
-      fontWeight: FontWeight.w500,
-      color: Color(0xFF2D3236),
-      fontSize: 24.0);
-  static TextStyle roleTextStyle() => const TextStyle(
-      fontFamily: AppFontStyle.font,
-      fontWeight: FontWeight.w600,
-      color: Color(0xFF6B6E71),
-      fontSize: 16.0);
-  static TextStyle informationHeaderTextStyle() => const TextStyle(
-      fontFamily: AppFontStyle.font,
-      fontWeight: FontWeight.w500,
-      color: Color(0xFF393E42),
-      fontSize: 20.0);
-  static TextStyle informationTitleTextStyle() => const TextStyle(
-      fontFamily: AppFontStyle.font,
-      fontWeight: FontWeight.w400,
-      color: Color(0xFF9C9FA1),
-      fontSize: 16.0);
-  static TextStyle informationDetailTextStyle() => const TextStyle(
-      fontFamily: AppFontStyle.font,
-      fontWeight: FontWeight.w400,
-      color: Color(0xFF393E42),
-      fontSize: 18.0);
+  static TextStyle informationHeaderTextStyle = AppFontStyle.wb80Md20;
+  static TextStyle informationTitleTextStyle = AppFontStyle.wb40R16;
+  static TextStyle informationDetailTextStyle = AppFontStyle.wb80R18;
+
   static EdgeInsets informationPadding =
       const EdgeInsets.fromLTRB(20, 16, 20, 16);
 
@@ -49,8 +29,8 @@ class UserProfileCard {
               Container(
                 decoration: const BoxDecoration(
                     border: Border(
-                        right:
-                            BorderSide(color: Color(0xFF9C9FA1), width: 1.0))),
+                        right: BorderSide(
+                            color: ColorConstant.whiteBlack40, width: 1.0))),
                 width: 300,
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
@@ -63,7 +43,7 @@ class UserProfileCard {
                       height: 24,
                     ),
                     DefaultTextStyle(
-                      style: nameTextStyle(),
+                      style: AppFontStyle.wb80Md24,
                       child: Text(
                         profileData["name"] + " " + profileData["surname"],
                         textAlign: TextAlign.center,
@@ -75,11 +55,11 @@ class UserProfileCard {
                     Container(
                       decoration: BoxDecoration(
                           border: Border.all(
-                              color: const Color(0xFFDADBDC), width: 1),
+                              color: ColorConstant.whiteBlack15, width: 1),
                           borderRadius: BorderRadius.circular(16.0),
-                          color: const Color(0xFFF3F3F3)),
+                          color: ColorConstant.whiteBlack5),
                       child: DefaultTextStyle(
-                          style: roleTextStyle(),
+                          style: AppFontStyle.wb60SemiB16,
                           child: Padding(
                             padding: const EdgeInsets.symmetric(
                                 vertical: 6, horizontal: 33),
@@ -107,14 +87,14 @@ class UserProfileCard {
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
                             DefaultTextStyle(
-                                style: informationHeaderTextStyle(),
+                                style: informationHeaderTextStyle,
                                 child: const Text("About me")),
                             const SizedBox(height: 8),
                             Padding(
                               padding: const EdgeInsets.symmetric(
                                   vertical: 8.0, horizontal: 16.0),
                               child: DefaultTextStyle(
-                                style: informationDetailTextStyle(),
+                                style: informationDetailTextStyle,
                                 child: Text(
                                   profileData["aboutMe"],
                                   maxLines: 5,
@@ -136,7 +116,7 @@ class UserProfileCard {
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
                             DefaultTextStyle(
-                                style: informationHeaderTextStyle(),
+                                style: informationHeaderTextStyle,
                                 child: const Text("User profile")),
                             const SizedBox(height: 8),
                             Padding(
@@ -148,28 +128,28 @@ class UserProfileCard {
                                       flex: 1,
                                       child: Text(
                                         "Gender",
-                                        style: informationTitleTextStyle(),
+                                        style: informationTitleTextStyle,
                                         textAlign: TextAlign.left,
                                       )),
                                   Expanded(
                                       flex: 3,
                                       child: Text(
                                         profileData["gender"],
-                                        style: informationDetailTextStyle(),
+                                        style: informationDetailTextStyle,
                                         textAlign: TextAlign.center,
                                       )),
                                   Expanded(
                                       flex: 1,
                                       child: Text(
                                         "Date of Birth",
-                                        style: informationTitleTextStyle(),
+                                        style: informationTitleTextStyle,
                                         textAlign: TextAlign.left,
                                       )),
                                   Expanded(
                                       flex: 3,
                                       child: Text(
                                         "412342342342",
-                                        style: informationDetailTextStyle(),
+                                        style: informationDetailTextStyle,
                                         textAlign: TextAlign.center,
                                       ))
                                 ],
@@ -185,7 +165,7 @@ class UserProfileCard {
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
                             DefaultTextStyle(
-                                style: informationHeaderTextStyle(),
+                                style: informationHeaderTextStyle,
                                 child: const Text("Contact")),
                             const SizedBox(height: 8),
                             Padding(
@@ -197,28 +177,28 @@ class UserProfileCard {
                                       flex: 1,
                                       child: Text(
                                         "Email",
-                                        style: informationTitleTextStyle(),
+                                        style: informationTitleTextStyle,
                                         textAlign: TextAlign.left,
                                       )),
                                   Expanded(
                                       flex: 3,
                                       child: Text(
                                         profileData["email"],
-                                        style: informationDetailTextStyle(),
+                                        style: informationDetailTextStyle,
                                         textAlign: TextAlign.center,
                                       )),
                                   Expanded(
                                       flex: 1,
                                       child: Text(
                                         "Phone",
-                                        style: informationTitleTextStyle(),
+                                        style: informationTitleTextStyle,
                                         textAlign: TextAlign.left,
                                       )),
                                   Expanded(
                                       flex: 3,
                                       child: Text(
                                         profileData["phone"],
-                                        style: informationDetailTextStyle(),
+                                        style: informationDetailTextStyle,
                                         textAlign: TextAlign.center,
                                       ))
                                 ],
