6868370488bbee737e64b6f1cd296cb039399f8b
NuttakitT <nuttakit.pliankunthod@gmail.com>
2023-03-09 14:42:38 +0700

    edit qurey msg to reply deasktop

--- lib/help_desk/help_desk_reply/view/widget/desktop/body_reply_desktop.dart
+++ lib/help_desk/help_desk_reply/view/widget/desktop/body_reply_desktop.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:senior_project/assets/color_constant.dart';
+import 'package:senior_project/core/datasource/firebase_services.dart';
 import 'package:senior_project/core/view_model/app_view_model.dart';
 import 'package:senior_project/help_desk/help_desk_reply/view/widget/desktop/chat_input_desktop.dart';
 import 'package:senior_project/help_desk/help_desk_reply/view/widget/desktop/description_desktop.dart';
 import 'package:senior_project/help_desk/help_desk_reply/view/widget/message.dart';
 import 'package:senior_project/help_desk/help_desk_reply/view_model/reply_channel_view_model.dart';
+
+Stream? query(String docId) {
+  return FirebaseServices("task").listenToSubDocument(docId, "replyChannel");
+}
 
 class BodyReplyDesktop extends StatefulWidget {
   const BodyReplyDesktop({super.key});
@@ -39,6 +44,9 @@ class _BodyReplyDesktopState extends State<BodyReplyDesktop> {
     double screenHeight = MediaQuery.of(context).size.height;
     int? role = context.watch<AppViewModel>().app.getUser.getRole; 
     String taskTitle = context.watch<ReplyChannelViewModel>().getTaskData["title"];
+    String docId = context.watch<ReplyChannelViewModel>().getTaskData["docId"];
+    String userId = context.watch<AppViewModel>().app.getUser.getId;
+    context.read<ReplyChannelViewModel>().clearModel();
 
     return Padding(
       padding: const EdgeInsets.only(right: 20),
@@ -89,14 +97,39 @@ class _BodyReplyDesktopState extends State<BodyReplyDesktop> {
                     decoration: const BoxDecoration(color: ColorConstant.white),
                     child: Padding(
                       padding: const EdgeInsets.symmetric(horizontal: 16),
-                      child: ListView.builder(
-                        itemCount: data.length,
-                        itemBuilder: (context, index) => Message(
-                          isSender:  data[index]["isSender"],
-                          text: data[index]["text"],
-                          isMobile: false,
-                        ),
-                      ),
+                      child: StreamBuilder(
+                        stream: query(docId),
+                        builder: (context, snapshot) {
+                          if (snapshot.connectionState == ConnectionState.active) {
+                            if (snapshot.data.docs.isNotEmpty) {
+                              context.read<ReplyChannelViewModel>().reconstructData(snapshot.data);
+                              List<Map<String, dynamic>> data = context.watch<ReplyChannelViewModel>().getReply(userId);
+                              return ListView.builder(
+                                itemCount: data.length,
+                                itemBuilder: (context, index) => Message(
+                                  isSender:  data[index]["isSender"],
+                                  text: data[index]["text"],
+                                  isMobile: false,
+                                ),
+                              );
+                            }
+                            return const Padding(
+                              padding: EdgeInsets.only(top: 16),
+                              child: Text("No messages in this task"),
+                            );
+                          }
+                          return Center(
+                            child: Container(
+                              width: 50,
+                              height: 50,
+                              decoration: const BoxDecoration(
+                                shape: BoxShape.circle
+                              ),
+                              child: const CircularProgressIndicator()
+                            )
+                          );
+                        },
+                      )
                     ),
                   ),
                   const ChatInputDesktop(),
