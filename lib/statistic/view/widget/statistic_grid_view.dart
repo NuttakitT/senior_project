4cbd5fb73704e76a94aa5f63582259cb2f558235
runnsss <runnsiriphuwanich@gmail.com>
2023-04-07 11:52:06 +0700

    Statistic View with date Filter

--- lib/statistic/view/widget/statistic_grid_view.dart
+++ lib/statistic/view/widget/statistic_grid_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:senior_project/statistic/model/statistic_model.dart';
+import 'package:senior_project/statistic/view/widget/date_filter.dart';
 
 class StatisticGridView extends StatefulWidget {
   final List<Chart> data;
@@ -13,12 +14,13 @@ class _StatisticGridViewState extends State<StatisticGridView> {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 64.0),
+      padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 64.0),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.start,
         // all the charts will be below here and each chart should be in each file in the widget folder.
         children: [
+          const DateFilter(),
           Row(children: []),
           Row(children: []),
           Row(children: []),
--- lib/statistic/view_model/statistic_view_model.dart
+++ lib/statistic/view_model/statistic_view_model.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 
 class StatisticViewModel extends ChangeNotifier {
-  final StatisticViewModel _model = StatisticViewModel();
   Future<StatisticViewModel> fetchPage() async {
-    return _model;
+    return this;
   }
 }
