552d9ad38abfc31f96d969a51990735babf131ed
runnsss <runnsiriphuwanich@gmail.com>
2023-06-01 15:37:33 +0700

    remove unused package.

--- lib/facility/view/admin_room/admin_room.dart
+++ lib/facility/view/admin_room/admin_room.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:senior_project/core/template/template_desktop/view/page/template_desktop.dart';
 import 'package:senior_project/facility/model/facility_model.dart';
-import 'package:senior_project/facility/view/admin_room/room_date_filter.dart';
 import 'package:senior_project/facility/view/admin_room/room_stat_table.dart';
 import 'package:senior_project/facility/view/widget/facility_header.dart';
 import 'package:senior_project/facility/view_model/facility_view_model.dart';
@@ -44,14 +43,8 @@ class AdminRoomStatisticView extends StatefulWidget {
 class _AdminRoomStatisticViewState extends State<AdminRoomStatisticView> {
   @override
   Widget build(BuildContext context) {
-    // DateTime from = DateTime(2023, 4, 15, 12, 0, 0);
-    // DateTime to = DateTime(2023, 5, 20, 12, 0, 0);
-
     return FutureBuilder(
-      future: context.watch<FacilityViewModel>().fetchBookingData(
-          // context.watch<FacilityViewModel>().startDate,
-          // context.watch<FacilityViewModel>().endDate
-          ),
+      future: context.watch<FacilityViewModel>().fetchBookingData(),
       builder: ((context, snapshot) {
         if (snapshot.hasError) {
           return Text('Has error ${snapshot.error}');
